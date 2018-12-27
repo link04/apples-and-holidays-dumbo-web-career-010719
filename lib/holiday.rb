bce63ed20e2c69179b4435fedff1afb37ea0985d
Maximo Bautista <max.bautista97@gmail.com>
2018-12-27 06:58:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,8 +73,13 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash.each do |key, value|
-    puts key.to_s.capitalize+":"
-    puts value.to_s.capitalize+":"
+    
+    puts "#{}key.to_s.capitalize"
+    
+    value.each do |key, value|
+      puts key.to_s.capitalize+":"
+    end
+   
    
   end
   
