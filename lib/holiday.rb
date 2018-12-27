23931ee5849cdb8f8192c5606f2e43534a1eb50e
Maximo Bautista <max.bautista97@gmail.com>
2018-12-27 07:34:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   
   
-  holiday_hash.collect do |key, value|
+  holiday_hash.each do |key, value|
     value.each do |key1, value1|
       if value1.include? "BBQ"
         return key1
