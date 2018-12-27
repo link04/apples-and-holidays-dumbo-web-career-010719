996f908c92f21fbdccfb78fdb44e751141ef6e71
Maximo Bautista <max.bautista97@gmail.com>
2018-12-27 06:40:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   
   holiday_hash[:winter].collect do |key, value|
-    [key][value]
+    [key]
   end
   
 end
