2d6cda7bdda1dc686f24d7049cbba26b30de9f76
Maximo Bautista <max.bautista97@gmail.com>
2018-12-27 06:31:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
   holiday_hash[:winter].each do |key, value|
-      [key] << supply
+     holiday_hash[:winter][key] << supply
   end
 
 end
