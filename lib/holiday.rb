603c413e77bb17aae58525d7989196289a41ec14
Maximo Bautista <max.bautista97@gmail.com>
2018-12-27 06:32:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # add the second argument to the memorial day array
   
   
-  holiday_hash[:spring][memorial_day] << supply
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
